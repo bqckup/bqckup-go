655d089dafbc8819e70f244c8995ddf444b7d855
Reja Revaldy F <rejarevaldy30@gmail.com>
2026-09-04 10:32:44 +0800

    v0.0.8

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -2,7 +2,7 @@ package buildinfo
 
 // Values are replaced by release builds through -ldflags.
 var (
-	version = "v0.0.7"
+	version = "v0.0.8"
 	commit  = ""
 )
 
--- internal/buildinfo/buildinfo_test.go
+++ internal/buildinfo/buildinfo_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestCurrentUsesDevelopmentDefaults(t *testing.T) {
 	info := Current()
-	if info.Version != "v0.0.7" {
-		t.Fatalf("version = %q, want v0.0.7", info.Version)
+	if info.Version != "v0.0.8" {
+		t.Fatalf("version = %q, want v0.0.8", info.Version)
 	}
 	if info.Commit != "" {
 		t.Fatalf("commit = %q, want empty", info.Commit)
